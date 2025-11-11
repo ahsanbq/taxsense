ecc8df925f3bde2fb1818a7282c0f384ec348d00
Ahsanul Karim <67556416+ahsantamim@users.noreply.github.com>
2025-11-11 13:30:06 +0600

    whats'app button added on main page

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import WhatsAppButton from "./WhatsAppButton";
 
 const Layout = ({ children }) => {
   return (
@@ -11,6 +12,7 @@ const Layout = ({ children }) => {
         {children}
       </main>
       <Footer />
+      <WhatsAppButton />
     </div>
   );
 };
