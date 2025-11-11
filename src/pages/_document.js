77d6bb23c1bd669dbf732f486810032782660967
Ahsanul Karim <67556416+ahsantamim@users.noreply.github.com>
2025-11-11 16:04:42 +0600

    favicon fixed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,27 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Favicons */}
+        <link rel="icon" type="image/x-icon" href="/favicon_io/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon_io/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_io/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+
         {/* Preconnect to external domains for performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
